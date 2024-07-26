f0923fac710f475ea930080b7d238922440be2b9
valetedd <vale98h@gmail.com>
2024-07-26 17:01:45 +0200

    data shit

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import json
 if __name__ == "__main__":
     with open("data.json", mode="r", encoding="utf-8") as d:
         data = json.load(d)
+    print(len(data))
     df = pd.DataFrame(data)
-
+    df["eventuality"] = df["eventuality"].apply(lambda x: f"{x['specific']} ({x['type']})")
+    print(df[df["eventuality"].str.contains("politician")])
+    
 
     
