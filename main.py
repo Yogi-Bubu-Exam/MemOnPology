a088b8b9df75e87ab0b50e0db556d749fe6b5a42
valetedd <vale98h@gmail.com>
2024-07-27 14:35:48 +0200

    added json files miao

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from pprint import pprint as pp
 import pandas as pd
 import json
 
-if __name__ == "__main__":
-    with open("data.json", mode="r", encoding="utf-8") as d:
-        data = json.load(d)
-    print(len(data))
-    df = pd.DataFrame(data)
-    df["eventuality"] = df["eventuality"].apply(lambda x: f"{x['specific']} ({x['type']})")
-    print(df[df["eventuality"].str.contains("politician")])
-    
+path = "cmm.json"
+with open(path, mode="r", encoding="utf-8") as d:
+    data = json.load(d)
+print(len(data))
+df = pd.DataFrame(data)
+df["eventuality"] = df["eventuality"].apply(lambda x: f"{x['specific']} ({x['type']})")
+query = df[["template", "connotation", "attitude", "opinion"]]
+print(query.describe())
+
 
     
