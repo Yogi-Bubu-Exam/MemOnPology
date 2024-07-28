d1fcb545f65d28d51badc23f33bb2346712ef292
valetedd <vale98h@gmail.com>
2024-07-28 15:08:00 +0200

    new json added

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from pprint import pprint as pp
 import pandas as pd
 import json
 
-path = "cmm.json"
+path = "pc.json"
 with open(path, mode="r", encoding="utf-8") as d:
     data = json.load(d)
 print(len(data))
 df = pd.DataFrame(data)
 df["eventuality"] = df["eventuality"].apply(lambda x: f"{x['specific']} ({x['type']})")
-query = df[["template", "connotation", "attitude", "opinion"]]
+query = df[["eventuality", "connotation", "attitude", "opinion"]]
 print(query.describe())
 
 
