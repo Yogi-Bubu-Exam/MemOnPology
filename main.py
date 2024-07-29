1cd6259d04f45434b42f0f8f288445a7f43186de
valetedd <vale98h@gmail.com>
2024-07-29 21:07:31 +0200

    minor changes to ontology and merged JSONs

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from pprint import pprint as pp
 import pandas as pd
 import json
 
-path = "comp.json"
+path = "data.json"
 with open(path, mode="r", encoding="utf-8") as d:
     data = json.load(d)
 print(len(data))
 df = pd.DataFrame(data)
+print(df["template"].unique())
 df["eventuality"] = df["eventuality"].apply(lambda x: f"{x['specific']} ({x['type']})")
-query = df[["eventuality", "connotation", "attitude", "opinion"]]
+query = df[["eventuality", "connotation", "attitude", "opinion"]][df["template"].str.contains("Political compass")]
 print(query.describe())
 
 
